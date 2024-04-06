60091823f435c9105ad095504781d3891adca038
Andrew Elmore <andrew.elmore93@gmail.com>
2024-04-06 19:59:24 -0400

    PreferencesDialog

---
+++ src/views/components/PreferencesEditor/PreferencesDialog.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Dialog, DialogActions, DialogContent, Button } from '@mui/material';
+import { useTheme, useMediaQuery } from '@mui/material';
+import { useMessage } from './../../context/MessageContext';
+import PreferenceEditor from './index';
+import Preference from '../../../domain/Preference';
+
+function PreferencesDialog({ open, onClose, onSave, initialPreference }) {
+  const theme = useTheme();
+  const { showMessage } = useMessage();
+  const [preference, setPreference] = useState(initialPreference || new Preference());
+
+  const handleSave = () => {
+    try {
+      preference.validate();
+      onSave(preference);
+      showMessage('Preferences Updated!', 'success');
+      onClose();
+    } catch (error) {
+      showMessage(error.message, 'error');
+    }
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
+      <DialogContent>
+          <PreferenceEditor preference={preference} setPreference={setPreference} />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={handleSave}>Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default PreferencesDialog;
